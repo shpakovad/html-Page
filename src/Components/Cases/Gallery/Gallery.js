c8aa465423569db2d8d73d39156ca663982621ac
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-05-07 20:21:47 +0300

    added some styles

--- src/Components/Cases/Gallery/Gallery.js
+++ src/Components/Cases/Gallery/Gallery.js
@@ -5,11 +5,11 @@ import Slider from "../Slider/Slider";
 function Gallery() {
     return (<>
             <section className={style.menu}>
-                <div className={style.wrapperLink_1}><a href="/" className={style.link_1}>Все мероприятия</a></div>
-                <div className={style.wrapperLink_2}><a href="/" className={style.link}>Конференции</a></div>
-                <div className={style.wrapperLink_3}><a href="/" className={style.link}>Бизнес завтраки</a></div>
-                <div className={style.wrapperLink_4}><a href="/" className={style.link}>Коучинги и тренинги</a></div>
-                <div className={style.wrapperLink_5}><a href="/" className={style.link}>Тимбилдинги и корпоративы</a>
+                <div className={style.wrapperLink_1}><a href="#" className={style.link_1}>Все мероприятия</a></div>
+                <div className={style.wrapperLink_2}><a href="#" className={style.link}>Конференции</a></div>
+                <div className={style.wrapperLink_3}><a href="#" className={style.link}>Бизнес завтраки</a></div>
+                <div className={style.wrapperLink_4}><a href="#" className={style.link}>Коучинги и тренинги</a></div>
+                <div className={style.wrapperLink_5}><a href="#" className={style.link}>Тимбилдинги и корпоративы</a>
                 </div>
             </section>
             <div className={style.gallery}>
